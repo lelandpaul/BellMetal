153466b10d4ef0caf4c279aeff06c606eb3224e0
lelandpaul <me@lelandpaul.com>
2023-07-30 15:33:01 -0400

    Initial commit

---
+++ Sources/BellMetal/Aliases.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Leland Paul Kusmer on 7/27/23.
+//
+
+import Foundation
---
+++ Tests/BellMetalTests/BlockTests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Leland Paul Kusmer on 7/30/23.
+//
+
+import Foundation
